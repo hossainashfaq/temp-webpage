c7f24646db468cfe1f0c0c14b7963ebec63d8f17
hossainashfaq <hossain.ashfaq@outlook.com>
2022-03-05 23:47:41 +0600

    updates

--- js/app.js
+++ js/app.js
@@ -3,13 +3,20 @@ const API_KEY = `36d4014a291f992e31a2c0e7cfd06195`;
 const searchTemp = () => 
 {
     const city = document.getElementById('city-input').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
         fetch(url)
         .then(res => res.json())
-        .then(data => displayTemp(data.name));
+        .then(data => displayTemp(data));
 }
+const setInnerText = (id, cityText) =>
+{
+    document.getElementById(id).innerText = cityText;
+}
+
 const displayTemp = (data) => 
 {
+    setInnerText('city-name', data.name);
+    setInnerText('show-temp', data.temp);
     console.log(data);
 }
